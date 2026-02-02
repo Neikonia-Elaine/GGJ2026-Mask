ea37ad4e74ee85028f2a46a709cac17918024c94
Neiko <zhiyuanf89@gmail.com>
2026-02-01 20:47:06 -0800

    fix bug

--- Assets/Game/Runtime/Gameplay/Interactable/DairyInteractor.cs
+++ Assets/Game/Runtime/Gameplay/Interactable/DairyInteractor.cs
@@ -5,22 +5,9 @@ using UnityEngine;
 
 public class DairyInteractor : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public override void Interact()
     {
         base.Interact();
         UIManager.Instance.Open<DairyPanel>();
-        
     }
 }
